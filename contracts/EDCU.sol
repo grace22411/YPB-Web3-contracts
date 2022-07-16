fd274a6416b1f4a9ac29caaff356183fd609de6a
grace22411 <olayinkagrace112.ob@gmail.com>
2022-07-16 21:19:26 +0400

    added mint function to token contract

--- contracts/EDCU.sol
+++ contracts/EDCU.sol
@@ -6,4 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Edcu is ERC20{
     constructor() ERC20("EDCU", "AXYZ LMS Token") {}
+
+    function mint(address _to, unit256 _amount) public {
+        _mint(_to, _amount);
+    }
 }
