fe02fedf965478bebdcaf02ff2aab4a2799aeb44
blackalbino01 <hauwal4969@gmail.com>
2022-07-26 22:14:03 +0100

    initial code refactored

--- contracts/Axyz.sol
+++ contracts/Axyz.sol
@@ -6,18 +6,37 @@ import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "./FractionalToken.sol";
 
-contract LMS is 
+contract Axyz is 
 Initializable,
 UUPSUpgradeable,
-OwnableUpgradeable, 
-ReentrancyGuardUpgradeable {
+AccessControlUpgradeable,
+ReentrancyGuardUpgradeable,
+ERC721URIStorageUpgradeable
+ {
     using SafeMathUpgradeable for uint256;
-    using SafeERC20Upgradeable for IERC20Upgradeable;
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+
+    CountersUpgradeable.Counter private _tokenId;
+
+    struct _fnft{
+        uint256 tokenId;
+        address fractionalToken;
+    }
+
+    event CourseCreated(
+        uint256 tokenId,
+        address facilitator
+    );
+
+    mapping(uint256 => _fnft) public FNFT;
 
 
     /// @notice Contract initializer
@@ -25,7 +44,11 @@ ReentrancyGuardUpgradeable {
         public
         initializer
     {
-        __Ownable_init();
+        __ERC721_init("Axyz Learning Management System", "AXYZ");
+        __AccessControl_init();
+        __UUPSUpgradeable_init();
+         _tokenId.increment();
+        _grantRole("Admin", msg.sender);
 
     }
 
@@ -34,7 +57,7 @@ ReentrancyGuardUpgradeable {
      * @notice Authorizes upgrade allowed to only proxy 
      * @param newImplementation the address of the new implementation contract 
      */
-    function _authorizeUpgrade(address newImplementation) internal override onlyOwner{}
+    function _authorizeUpgrade(address newImplementation) internal override onlyRole("Admin"){}
 
     function purchaseToken()external{}
 
@@ -44,6 +67,34 @@ ReentrancyGuardUpgradeable {
 
     function getCertificate(address _user)external{}
 
-    function createCourse()external{}
+    function createCourse(
+        address _to,
+        string memory _name,
+        string memory _code
+    ) external{
+        uint256 newTokenId = _tokenId.current();
+        _safeMint(_to, newTokenId);
+
+        //Create a ERC20 Token Contract for this newly minted NFT
+        FractionalToken _fnftoken = (new FractionalToken)(_name, _code);                                     
+        _fnft memory fnft;                                                        
+        fnft.tokenId = _tokenId.current();                           
+        fnft.fractionalToken = address(_fnftoken);
+        FNFT[_tokenId.current()]  = fnft; 
+
+        _tokenId.increment();
+
+        emit CourseCreated(newTokenId, _to);
+    }
+
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721Upgradeable, AccessControlUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
     
 }
