499b9b014129294d3dd890bb78d85cf2bdca21e0
blackalbino01 <hauwal4969@gmail.com>
2022-07-27 00:17:03 +0100

    update

--- contracts/Axyz.sol
+++ contracts/Axyz.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
@@ -22,32 +23,51 @@ ReentrancyGuardUpgradeable,
 ERC721URIStorageUpgradeable
  {
     using SafeMathUpgradeable for uint256;
+    using SafeERC20Upgradeable for IERC20Upgradeable;
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
-    CountersUpgradeable.Counter private _tokenId;
+    CountersUpgradeable.Counter private _courseId;
 
-    struct _fnft{
-        uint256 tokenId;
+    IERC20Upgradeable public EDCUToken;
+
+    struct Course{
+        uint256 courseId;
+        uint256 price;
         address fractionalToken;
     }
 
     event CourseCreated(
-        uint256 tokenId,
+        uint256 courseId,
+        uint256 price,
         address facilitator
     );
 
-    mapping(uint256 => _fnft) public FNFT;
+    event FractionalTokenPurchased(
+        uint256 tokenId,
+        uint256 price,
+        address indexed beneficiary
+    );
+
+    event EDCUTokenPurchased(
+        uint256 price,
+        address indexed beneficiary
+    );
+
+    mapping(uint256 => Course) public Courses;
+
+    mapping(uint256 => uint256) public tokenPrices;
 
 
     /// @notice Contract initializer
-    function initialize()
+    function initialize(address _edcu)
         public
         initializer
     {
         __ERC721_init("Axyz Learning Management System", "AXYZ");
         __AccessControl_init();
         __UUPSUpgradeable_init();
-         _tokenId.increment();
+        EDCUToken = IERC20Upgradeable(_edcu);
+         _courseId.increment();
         _grantRole("Admin", msg.sender);
 
     }
@@ -59,9 +79,25 @@ ERC721URIStorageUpgradeable
      */
     function _authorizeUpgrade(address newImplementation) internal override onlyRole("Admin"){}
 
-    function purchaseToken()external{}
+    function purchaseFToken(uint courseId, address _token)external{
+        Course memory course = Courses[courseId];
+        IERC20Upgradeable(_token).safeTransferFrom(msg.sender, address(this), course.price);
+        //IERC20Upgradeable(fnft.fractionalToken).mint(msg.sender, 10 ether);
 
-    function NFTEnroll()external{}
+        emit FractionalTokenPurchased(courseId, course.price, msg.sender);
+    }
+
+    function purchaseEDCUToken(uint _amount, address _token) external{
+        uint price = tokenPrices[_amount];
+        IERC20Upgradeable(_token).safeTransferFrom(msg.sender, address(this), _amount);
+        EDCUToken.safeTransfer(msg.sender, price);
+
+        emit EDCUTokenPurchased(price, msg.sender);
+    }
+
+    function NFTEnroll(uint courseId)external{
+
+    }
 
     function TokenEnroll()external{}
 
@@ -69,22 +105,24 @@ ERC721URIStorageUpgradeable
 
     function createCourse(
         address _to,
+        uint price,
         string memory _name,
         string memory _code
     ) external{
-        uint256 newTokenId = _tokenId.current();
+        uint256 newTokenId = _courseId.current();
         _safeMint(_to, newTokenId);
 
         //Create a ERC20 Token Contract for this newly minted NFT
-        FractionalToken _fnftoken = (new FractionalToken)(_name, _code);                                     
-        _fnft memory fnft;                                                        
-        fnft.tokenId = _tokenId.current();                           
-        fnft.fractionalToken = address(_fnftoken);
-        FNFT[_tokenId.current()]  = fnft; 
+        FractionalToken _fnftoken = new FractionalToken(_name, _code);                                     
+        Course memory course;                                                        
+        course.courseId = newTokenId;                           
+        course.fractionalToken = address(_fnftoken);
+        course.price = price;
+        Courses[newTokenId]  = course; 
 
-        _tokenId.increment();
+        _courseId.increment();
 
-        emit CourseCreated(newTokenId, _to);
+        emit CourseCreated(newTokenId, price,  _to);
     }
 
 
